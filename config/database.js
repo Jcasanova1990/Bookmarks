2e231e248175212f09fbe58108d022c2c2d4853c
Jcasanova1990 <jeremykcasanova@gmail.com>
2024-02-15 11:16:45 -0500

    Created setup init and completed config

---
+++ config/database.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGO_URI)
+
+const db = mongoose.connection
+
+db.on('connected', () => {
+    console.log(`connected to ${db.name} at ${db.host}. Im HIM, the MERN Hokage`)
+})
